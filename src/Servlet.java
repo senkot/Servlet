c3330b62a255e4086a0158631758fa1f048a4c42
senko88 <kostyanchik88@gmail.com>
2020-05-29 16:01:05 +0300

    use session

--- src/Servlet.java
+++ src/Servlet.java
@@ -1,5 +1,7 @@
+import somePackage.Cart;
+
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
@@ -7,17 +9,33 @@ public class Servlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        Cart cart = (Cart)session.getAttribute("cart");
+
         String name = request.getParameter("name");
-        String lastName = request.getParameter("lastName");
+        int amount = Integer.parseInt(request.getParameter("amount"));
 
-        PrintWriter printWriter = response.getWriter();
+        if(cart == null) {
+            cart = new Cart();
+            cart.setName(name);
+            cart.setAmount(amount);
+        }
 
-        printWriter.println("<html>");
-        printWriter.println("<title>");
-        printWriter.println("Servlet!");
-        printWriter.println("</title>");
-        printWriter.println("<h1> Hello, " + name + "! Mr. " + lastName + ", greetings! </h1>");
-        printWriter.println("<p style='size:5px; background-color: lightblue; text-align: center; color: green;'> this is Tomcat-response </p>");
-        printWriter.println("</html>");
+        session.setAttribute("cart", cart);
+
+
+
+//        PrintWriter printWriter = response.getWriter();
+//        printWriter.println("<html>");
+//        printWriter.println("<title>");
+//        printWriter.println("Servlet!");
+//        printWriter.println("</title>");
+//        printWriter.println("<h1> Your count is : " + count + " </h1>");
+//        printWriter.println("<p style='size:5px; background-color: lightblue; text-align: right; color: green;'> this is Tomcat-response </p>");
+//        printWriter.println("</html>");
+
+        getServletContext().getRequestDispatcher("/showCart.jsp").forward(request,response);
+
     }
 }
